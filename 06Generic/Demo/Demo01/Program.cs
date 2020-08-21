c38c905bcee965a3ab6e8158f98025e337dd99a4
laishilin <laishilin@ezgrid.cn>
2020-08-21 19:09:34 +0800

    实例158-160

---
+++ 06Generic/Demo/Demo01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+ *泛型约束
+ */
+namespace Demo01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Test<int, Person> t = new Test<int, Person>();
+            t.start();
+        }
+    }
+
+    internal class Test<T, K>
+        where T : struct
+        where K : class, new()
+    {
+        public void start()
+        {
+            string CheckType(Type type) => type.FullName;
+            Console.WriteLine($"T的类型是 {CheckType(typeof(T))}");
+            Console.WriteLine($"K的类型是 {CheckType(typeof(K))}");
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+        }
+    }
+}
